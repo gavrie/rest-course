21bbb80c7c013576a0b8f3eaa6b8232869081c19
Gavrie Philipson <gavrie@philipson.co.il>
2021-07-29 16:47:17 +0300

    Lesson 1

---
+++ README.md
@@ -0,0 +1,23 @@
+## Preparing for development
+
+1. Make sure you have Python 3.9 or higher installed (it's best to use [pyenv](https://github.com/pyenv/pyenv#installation) for that, but you can use your system's Python 3.9 if you have to).
+2. Install [Poetry](https://python-poetry.org/docs/#installation) to manage dependencies.
+3. Have Poetry install the project's dependencies (this will create a new `virtualenv` automatically):
+
+       poetry install
+
+
+## Running
+
+    uvicorn main:app --reload
+
+
+## Testing via the API
+
+Creating a BDB:
+
+    $ httpx http://localhost:8000/bdbs -j '{"name":"foo","memory_size":2}'
+
+Listing the BDBs:
+
+    $ httpx http://localhost:8000/bdbs                    ~/source/redislabs/rest-course
---
+++ rest_course/api.py
@@ -0,0 +1,43 @@
+# PEP 585
+from collections.abc import Iterable
+
+from fastapi import FastAPI, HTTPException
+
+from .params import BDBParams
+from .types import BDB, UID
+
+app = FastAPI()
+
+all_bdbs: dict[UID, BDB] = {}
+bdb_last_uid = 0
+
+
+@app.post("/bdbs")
+def create_bdb(req: BDBParams) -> BDB:
+    global bdb_last_uid
+    bdb_last_uid += 1
+    uid = UID(bdb_last_uid)
+
+    bdb = BDB(
+        uid=uid,
+        name=req.name,
+        type=req.type,
+        memory_size=req.memory_size,
+    )
+
+    all_bdbs[uid] = bdb
+    return bdb
+
+
+@app.get("/bdbs/{uid}")
+def get_bdb(uid: UID) -> BDB:
+    try:
+        return all_bdbs[uid]
+    except LookupError:
+        raise HTTPException(status_code=404)
+
+
+@app.get("/bdbs")
+def get_all_bdbs() -> Iterable[BDB]:
+    for uid in all_bdbs:
+        yield all_bdbs[uid]
